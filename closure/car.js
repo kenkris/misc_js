e73e209edc530abb2634d07a88453c9ccdde36d5
Kenneth Kristensen <kekr@SCANMARKET.local>
2019-04-05 11:37:10 +0200

    Add more test data for closure test

--- closure/car.js
+++ closure/car.js
@@ -16,8 +16,10 @@ function createCar(brand, model){
 
 let car1 = createCar("Peugeot", "208");
 let car2 = createCar("VW", "Golf");
+let car3 = createCar("Ferrari", "F40")(5000000);
 
 car1(1000);
 car2(2000);
+
 
 
