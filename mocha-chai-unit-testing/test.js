f20123b4a31343bf5617ba7dabfc9676e3f8524d
Kenneth Kristensen <kekr@SCANMARKET.local>
2019-04-09 10:38:34 +0200

    Add before each test example

--- mocha-chai-unit-testing/test.js
+++ mocha-chai-unit-testing/test.js
@@ -6,8 +6,12 @@ let expect = chai.expect;
 
 chai.should();
 
-isEven = (value) => {
+isEven = value => {
     return value % 2 === 0;
+}
+
+addNumbers = (num1, num2) => {
+    return num1 + num2;
 }
 
 describe("isEven", () => {
@@ -17,5 +21,23 @@ describe("isEven", () => {
 
     it("should return false when value is odd", () => {
         expect(isEven(3)).to.be.false;
+    });
+});
+
+describe("addNumbers", () =>{
+    let num;
+
+    // reset init value for each assertion
+    beforeEach(() => {
+        num = 10;
+    });
+
+    it("should return 15 when adding 5 to 10", () => {
+        num = addNumbers(num, 5);
+        num.should.equal(15);
+    });
+    it("should return 23 when adding 13 to 10", () => {
+        num = addNumbers(num, 13);
+        num.should.equal(23);
     });
 });
