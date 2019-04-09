ab91039c91b7fb5843190077506a2bf0b2f0ee4c
Kenneth Kristensen <kekr@SCANMARKET.local>
2019-04-09 10:27:22 +0200

    Add mocha / chai test

---
+++ mocha-chai-unit-testing/test.js
@@ -0,0 +1,21 @@
+'use-strict'
+
+let chai = require('chai');
+let expect = chai.expect;
+
+
+chai.should();
+
+isEven = (value) => {
+    return value % 2 === 0;
+}
+
+describe("isEven", () => {
+    it("should return true when value is even", () => {
+        isEven(8).should.be.true;
+    });
+
+    it("should return false when value is odd", () => {
+        expect(isEven(3)).to.be.false;
+    });
+});
